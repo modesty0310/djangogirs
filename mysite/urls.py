9fbe85c6814a5a8d485c42f0f2302bac64170ddf
modesty0310 <rlatprua4@gmail.com>
2021-10-07 17:51:41 +0900

    로그인, 로그아웃

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,10 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
+from django.contrib.auth import views
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/login/', views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', views.LogoutView.as_view(),
+         name='logout', kwargs={'next_page': '/'}),
     path('', include('blog.urls')),
 ]
 
